c5edcb0ac497263412a4fad80519dace595adc0d
JIMMIE HAYAKAWA <jimmiehayakawa@JIMMIEnoMacBook-Air.local>
2026-08-21 17:30:02 +0900

    Fix: 字幕fontsize=60・MarginL/R=80（はみ出し解消）

--- scripts/gen_subtitle_filter.py
+++ scripts/gen_subtitle_filter.py
@@ -43,7 +43,7 @@ ScaledBorderAndShadow: yes
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,{fontname},72,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,3,0,2,40,40,780,1
+Style: Default,{fontname},60,&H00FFFFFF,&H000000FF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,3,0,2,80,80,780,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
