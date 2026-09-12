b0744909b3bdea6ae127d73be8619d53bbd0b259
aiconduit <jimmiehayakawa@gmail.com>
2026-09-12 13:42:58 +0900

    feat: RootPart1.tsx S01-S08

--- remotion-videos/documentary-30min/src/RootPart1.tsx
+++ remotion-videos/documentary-30min/src/RootPart1.tsx
@@ -5,21 +5,13 @@ import React from "react";
 
 const f = (s: number) => Math.round(s * VIDEO_FPS);
 const T = TRANSITION_FRAMES;
-
 const totalFrames =
-  f(SECTION_DURATION.s01_intro) +
-  f(SECTION_DURATION.s02_origin_openai) +
-  f(SECTION_DURATION.s03_origin_anthropic) +
-  f(SECTION_DURATION.s04_split) -
-  T * 3;
+  f(SECTION_DURATION.s01_hook) + f(SECTION_DURATION.s02_intro) +
+  f(SECTION_DURATION.s03_openai_birth) + f(SECTION_DURATION.s04_chatgpt) +
+  f(SECTION_DURATION.s05_anthropic_birth) + f(SECTION_DURATION.s06_split_reason) +
+  f(SECTION_DURATION.s07_safety_vs_growth) + f(SECTION_DURATION.s08_models_claude) - T * 7;
 
 export const RemotionRoot: React.FC = () => (
-  <Composition
-    id="DocumentaryPart1"
-    component={DocumentaryPart1}
-    durationInFrames={totalFrames}
-    fps={VIDEO_FPS}
-    width={VIDEO_WIDTH}
-    height={VIDEO_HEIGHT}
-  />
+  <Composition id="DocumentaryPart1" component={DocumentaryPart1}
+    durationInFrames={totalFrames} fps={VIDEO_FPS} width={VIDEO_WIDTH} height={VIDEO_HEIGHT} />
 );
